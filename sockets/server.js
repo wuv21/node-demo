feba42236126d5c52ded078e4d68e4146762b1f0
Vincent Wu <wuv21@uw.edu>
2015-12-03 17:15:58 -0800

    class code

--- sockets/server.js
+++ sockets/server.js
@@ -3,3 +3,45 @@
     main server script for the socket.io chat demo
 */
 
+var net = require('net');
+
+var server = net.createServer();
+
+var clients = [];
+
+server.on('connection', function(socket) {
+    var name;
+
+    function broadcast(name, message) {
+        clients.forEach(function(client) {
+            if (client !== socket) {
+                client.write('[' + name + '] ' + message);
+            }
+        });
+    }
+
+    clients.push(socket);
+
+    socket.write('Hello! What is your name?\n');
+
+    socket.on('data', function(data) {
+        if (!name) {
+            name = data.toString().trim();
+            socket.write('Hello ' + name + '!\n');
+        } else {
+            var message = data.toString();
+
+            if (message.trim() === 'exit') {
+                socket.end()
+            } else {
+                broadcast(name, data.toString());
+            }
+        }
+    });
+});
+
+server.on('listening', function() {
+    console.log('server listening on port 3000');
+});
+
+server.listen(3000);
